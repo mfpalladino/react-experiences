7424c92d2d5962e10ebb0f7d923971832413dbe1
Marcelo Palladino <mpalladino@directtalk.com.br>
2016-09-09 19:29:13 -0300

    flex and Germany flag.

--- app/components/Root.js
+++ app/components/Root.js
@@ -1,61 +1,18 @@
 import React, { Component } from 'react';
-import { Modal, Text, TouchableHighlight, View } from 'react-native';
-
+import { View } from 'react-native';
 
 export default class Root extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { modalVisible: false };
-    }
-
-    setModalVisible(visible) {
-        this.setState({ modalVisible: visible });
-    }
-
     render() {
         return (
-            <View style={{ marginTop: 22 }}>
-                <Modal
-                    animationType={"slide"}
-                    transparent={false}
-                    visible={this.state.modalVisible}
-                    onRequestClose={() => { alert("Modal has been closed.") } }
-                    >
-                    <View style={{ marginTop: 22 }}>
-                        <View>
-                            <Text>Hello World no formulário MODAL!</Text>
-
-                            <TouchableHighlight onPress={() => {
-                                this.setModalVisible(!this.state.modalVisible)
-                            } }>
-                                <Text>Hide Modal</Text>
-                            </TouchableHighlight>
-
-                        </View>
-                    </View>
-                </Modal>
-
-                <TouchableHighlight onPress={() => {
-                    this.setModalVisible(true)
-                } }>
-                    <Text>Show Modal</Text>
-                </TouchableHighlight>
-
-            </View>)
+            // Try removing the `flex: 1` on the parent View.
+            // The parent will not have dimensions, so the children can't expand.
+            // What if you add `height: 300` instead of `flex: 1`?
+            <View style={{flex: 1}}>
+                <View style={{flex: 1, backgroundColor: 'black'}} />
+                <View style={{flex: 1, backgroundColor: 'red'}} />
+                <View style={{flex: 1, backgroundColor: 'yellow'}} />
+            </View>
+        )
     }
 }
-
-// const styles = StyleSheet.create({
-
-//     container: {
-//         paddingTop: 64,
-//         flex: 1,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         backgroundColor: Colors.white
-//     },
-//     searchBox: {
-//         width: Consts.windowWidth * 0.8
-//     }
-// })
