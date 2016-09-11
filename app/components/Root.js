4c65d9b95285b1298712729a3a9e0615f329797b
Marcelo Palladino <mpalladino@directtalk.com.br>
2016-09-10 23:20:34 -0300

    handle text input.

--- app/components/Root.js
+++ app/components/Root.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default class Root extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { textoDigitado: '' };
+    }
+
     render() {
         return (
-            <View >
-                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'green' }}/>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}/>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}/>
-                </View>
-                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch' }}>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'green' }}/>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}/>
-                    <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}/>
-                </View>
+            <View style={{ padding: 10 }}>
+                <TextInput
+                    style={{ height: 40 }}
+                    placeholder="Exemplo de placeHolder..."
+                    onChangeText={(text) => this.setState({ textoDigitado : text }) }
+                    />
+                <Text style={{ padding: 10, fontSize: 42 }}>
+                    {this.state.textoDigitado }
+                </Text>
             </View>
         )
     }
