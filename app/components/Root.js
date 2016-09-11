9b8bf652bd40c7b01e81a2732df0c4c7e2bd43cb
Marcelo Palladino <mpalladino@directtalk.com.br>
2016-09-10 23:04:03 -0300

    Layout and flex.

--- app/components/Root.js
+++ app/components/Root.js
@@ -5,13 +5,17 @@ export default class Root extends Component {
 
     render() {
         return (
-            // Try removing the `flex: 1` on the parent View.
-            // The parent will not have dimensions, so the children can't expand.
-            // What if you add `height: 300` instead of `flex: 1`?
-            <View style={{flex: 1}}>
-                <View style={{flex: 1, backgroundColor: 'black'}} />
-                <View style={{flex: 1, backgroundColor: 'red'}} />
-                <View style={{flex: 1, backgroundColor: 'yellow'}} />
+            <View >
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'green' }}/>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}/>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}/>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'stretch' }}>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'green' }}/>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'yellow' }}/>
+                    <View style={{ width: 50, height: 50, backgroundColor: 'blue' }}/>
+                </View>
             </View>
         )
     }
