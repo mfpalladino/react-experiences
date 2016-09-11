3d2697b7bc25009a8ae2fab09c2afa2d4f8c3fa6
Marcelo Palladino <mpalladino@directtalk.com.br>
2016-09-10 23:33:00 -0300

    ScrollView

--- app/components/Root.js
+++ app/components/Root.js
@@ -1,25 +1,91 @@
 import React, { Component } from 'react';
-import { Text, TextInput, View } from 'react-native';
+import { ScrollView, Image, Text, View } from 'react-native';
 
 export default class Root extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { textoDigitado: '' };
-    }
-
     render() {
         return (
-            <View style={{ padding: 10 }}>
-                <TextInput
-                    style={{ height: 40 }}
-                    placeholder="Exemplo de placeHolder..."
-                    onChangeText={(text) => this.setState({ textoDigitado : text }) }
-                    />
-                <Text style={{ padding: 10, fontSize: 42 }}>
-                    {this.state.textoDigitado }
-                </Text>
-            </View>
+        <ScrollView>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+          <Text style={{fontSize:96}}>Teste para rolagem</Text>
+        </ScrollView>
         )
     }
 }
